0b8ed44ab2beaf6003918c96406e71133800b000
UnNomOriginal <125469473+UnNomOriginal@users.noreply.github.com>
2023-03-06 10:17:56 -0500

    Calibrations des moteurs (maitre)

--- AvancementFourmie/Maitre.c
+++ AvancementFourmie/Maitre.c
@@ -1,13 +1,13 @@
 //Serveur Bluetooth Fourmi
 #include "BluetoothSerial.h"
-#include "esp_bt_device.h"
-#include <ESP32Servo.h>
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
+#include <ESP32Servo.h>
 
 BluetoothSerial SerialBT;
 const String deviceName = "Fourmi";
+String blueetoothSendMsg;
 
 //setup le port 2 comme maitre (envoyer des informations)
 HardwareSerial Maitre(2);
@@ -27,16 +27,16 @@ Servo Gar3;
 
 //Mise en place servo moteur GAV/GARR/DM1
 #define pinServoGav1 4
-#define pinServoGav2 5
-#define pinServoGav3 6
+#define pinServoGav2 16
+#define pinServoGav3 17
 
-#define pinServoDm1 7
-#define pinServoDm2 8
-#define pinServoDm3 9
+#define pinServoDm1 25
+#define pinServoDm2 33
+#define pinServoDm3 32
 
-#define pinServoGar1 10
-#define pinServoGar2 11
-#define pinServoGar3 12
+#define pinServoGar1 5
+#define pinServoGar2 18
+#define pinServoGar3 19
 
 //define les Pin qu'on va utilier pour transmettre des informations
 //Tx : transmetteur doit être branché sur un receveur de l'autre ESP32
@@ -51,7 +51,27 @@ int indiceGauch3 = 0;
 int indiceGauch4 = 0;
 
 int avancerD = 0;
-int vitesse = 30;
+int vitesse = 90;
+
+int test=0;
+  int posBaseGav1=110;
+  int posBaseGav2=145;
+  int posBaseGav3=165;
+  
+  int posBaseDm1=95;
+  int posBaseDm2=35;
+  int posBaseDm3=15;
+
+  int posBaseGar1=85;
+  int posBaseGar2=145;
+  int posBaseGar3=165;
+
+//-------------------------Variables Temporaires ci-dessous------------------------
+
+
+
+
+
 
 void setup() {
   //initialise console
@@ -70,17 +90,17 @@ void setup() {
   ESP32PWM::allocateTimer(2);
   ESP32PWM::allocateTimer(3);
   
-  Gav1.setPeriodHertz(30);
-  Gav2.setPeriodHertz(30);
-  Gav3.setPeriodHertz(30);
+  Gav1.setPeriodHertz(50);
+  Gav2.setPeriodHertz(50);
+  Gav3.setPeriodHertz(50);
   
-  Dm1.setPeriodHertz(30);
-  Dm2.setPeriodHertz(30);
-  Dm3.setPeriodHertz(30);
+  Dm1.setPeriodHertz(50);
+  Dm2.setPeriodHertz(50);
+  Dm3.setPeriodHertz(50);
   
-  Gar1.setPeriodHertz(30);
-  Gar2.setPeriodHertz(30);
-  Gar3.setPeriodHertz(30);
+  Gar1.setPeriodHertz(50);
+  Gar2.setPeriodHertz(50);
+  Gar3.setPeriodHertz(50);
   
   Gav1.attach(pinServoGav1, 500, 2400);
   Gav2.attach(pinServoGav2, 500, 2400);
@@ -92,25 +112,52 @@ void setup() {
 
   Gar1.attach(pinServoGar1, 500, 2400);
   Gar2.attach(pinServoGar2, 500, 2400);
-  Gar3.attach(pinServoGav3, 500, 2400);
+  Gar3.attach(pinServoGar3, 500, 2400);
+
+  //-------------------position de base des pattes au démarrage--------------------
+  Gav1.write(posBaseGav1);
+  Gav2.write(posBaseGav2);
+  Gav3.write(posBaseGav3);
+
+  Dm1.write(posBaseDm1);
+  Dm2.write(posBaseDm2);
+  Dm3.write(posBaseDm3);
+
+  Gar1.write(posBaseGar1);
+  Gar2.write(posBaseGar2);
+  Gar3.write(posBaseGar3);
+
+
+  Serial.println("Starting Loop in 3 sec");
+  delay(3000);
+  
 }
 
 
 
 void loop() {
-  String blueetoothSendMsg;
   if (SerialBT.available()) {
     blueetoothSendMsg = SerialBT.readString();
     blueetoothSendMsg.trim();
+    Serial.print("Bluetooth : ");
+    Serial.println(blueetoothSendMsg);
   }
+
+  
+blueetoothSendMsg = "avancer";
+
+
+
 
   if (blueetoothSendMsg == "avancer") {
     //tu codes en else if car l'avancement se fait petit pas par petit pas la boucle utilise le boucle du programme principal
     if (!avancerD) {
       avancerG();
+      Serial.println("Avancer Gauche");
     } else {
-      Maitre.print("avancer");
+      Maitre.print("avancer;");     // Envoyer commande avec ; à la fin
       avancerG();
+      Serial.println("Avancer Droite et gauche");
     }
   }
 
@@ -118,38 +165,41 @@ void loop() {
 }
 
 void avancerG() {
+  
   moveLegGav();
-  moveLegGar();
   moveLegDm();
+  moveLegGar();
+  
 }
 
 //C'est cette patte qui donne le rythme
 void moveLegGav() {
   // Rise the leg
   if (indiceGauch1 <= 10) {
-    Gav3.write(60 - indiceGauch1 * 2);
-    Gav2.write(90 - indiceGauch1 * 3);
+    Gav3.write(posBaseGav3 + (indiceGauch1*2));
+    Gav2.write(posBaseGav2 + (indiceGauch1*3));
     indiceGauch1++;
   }
   // Rotate the leg
   if (indiceGauch2 <= 30) {
-    Gav1.write(100 - indiceGauch2);
+    Gav1.write(posBaseGav1 + indiceGauch2);
     indiceGauch2++;
   }
   // Move back to touch the ground
   if (indiceGauch2 > 20 & indiceGauch3 <= 10) {
-    Gav3.write(40 + indiceGauch3 * 2);
-    Gav2.write(60 + indiceGauch3 * 3);
+    Gav3.write(posBaseGav3 - (indiceGauch3*2));
+    Gav2.write(posBaseGav2 - (indiceGauch3*3));
     indiceGauch3++;
   }
   // Stance phase - move leg while touching the ground
   // Rotate back to initial position
   if (indiceGauch2 >= 30) {
-    Gav1.write(70 + indiceGauch4);
+    Gav1.write((posBaseGav1+30) - indiceGauch4);    //Gav1 est à sa (posBase+30) pcq elle est à la position avancée. On la rétracte vers sa posBase (-30)
     indiceGauch4++;
     //ici il envoie l'information de bouger la droite!!
     avancerD = 1;
   }
+  
   // Reset the counters for repeating the process
   if (indiceGauch4 >= 30) {
     indiceGauch1 = 0;
@@ -163,47 +213,49 @@ void moveLegGav() {
 void moveLegGar() {
   //rise leg
   if (indiceGauch1 <= 10) {
-    Gar3.write(50 - indiceGauch1 * 2);
-    Gar2.write(80 - indiceGauch1 * 3);
+    Gar3.write(posBaseGar3 + (indiceGauch1*2));
+    Gar2.write(posBaseGar2 + (indiceGauch1*3));
   }
 
   //rotate vers l'avant
   if (indiceGauch2 <= 30) {
-    Gar1.write(80 - indiceGauch2);
+    Gar1.write(posBaseGar1 + indiceGauch2);
   }
 
   //en bas
   if (indiceGauch2 > 20 & indiceGauch3 <= 10) {
-    Gar3.write(30 + indiceGauch3 * 2);
-    Gar2.write(50 + indiceGauch3 * 3);
+    Gar3.write(posBaseGar3 - (indiceGauch3*2));
+    Gar2.write(posBaseGar2 - (indiceGauch3*3));
+    Serial.println("bas G");
   }
 
   //rotate vers l'arrière
   if (indiceGauch2 >= 30) {
-    Gar1.write(50 + indiceGauch4);
+    Gar1.write((posBaseGar1+30) - indiceGauch4);  //Gar1 est à sa (posBase+30) pcq elle est à la position avancée. On la rétracte vers sa posBase (-30)
   }
 }
 
 void moveLegDm() {
   //haut
   if (indiceGauch1 <= 10) {
-    Dm3.write(80 + indiceGauch1 * 2);
-    Dm2.write(50 + indiceGauch1 * 3);
+    Dm3.write(posBaseDm3 - (indiceGauch1*2));
+    Dm2.write(posBaseDm2 - (indiceGauch1*3));
   }
 
   //rotate ++
   if (indiceGauch2 <= 30) {
-    Dm1.write(30 + indiceGauch2);
+    Dm1.write(posBaseDm1 - indiceGauch2);
   }
 
   //bas
   if (indiceGauch2 > 20 & indiceGauch3 <= 10) {
-    Dm3.write(100 - indiceGauch3 * 2);
-    Dm2.write(80 - indiceGauch3 * 3);
+    Dm3.write(posBaseDm3 + (indiceGauch3*2));
+    Dm2.write(posBaseDm2 + (indiceGauch3*3));
+    Serial.println("bas D");
   }
 
   //rotate --
   if (indiceGauch2 >= 30) {
-    Dm1.write(60 - indiceGauch4);
+    Dm1.write((posBaseDm1-30) + indiceGauch4);  //Dm1 est à sa (posBase+30) pcq elle est à la position avancée. On la rétracte vers sa posBase (-30)
   }
 }
