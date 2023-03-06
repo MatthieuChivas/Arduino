b0546aa083ea0d4df4b13bb0dc0d6400c1213b8c
UnNomOriginal <125469473+UnNomOriginal@users.noreply.github.com>
2023-03-06 10:18:57 -0500

    Calibrations des moteurs (esclave)

--- AvancementFourmie/Esclave.c
+++ AvancementFourmie/Esclave.c
@@ -1,3 +1,4 @@
+
 #include <ESP32Servo.h> 
 
 HardwareSerial Esclave(1);
@@ -6,8 +7,8 @@ HardwareSerial Esclave(1);
 Servo Dav1;
 Servo Dav2;
 Servo Dav3;
-// TEST modification
-Servo Gm5;
+
+Servo Gm1;
 Servo Gm2;
 Servo Gm3;
 
@@ -16,17 +17,17 @@ Servo Dar2;
 Servo Dar3;
 
 //Mise en place servo moteur GM/DAV/DAR
-#define pinServoDav1 7
-#define pinServoDav2 8
-#define pinServoDav3 9
+#define pinServoDav1 5
+#define pinServoDav2 17
+#define pinServoDav3 16
 
-#define pinServoGm1 4
-#define pinServoGm2 5
-#define pinServoGm3 6
+#define pinServoGm1 27
+#define pinServoGm2 14
+#define pinServoGm3 12
 
-#define pinServoDar1 10
-#define pinServoDar2 11
-#define pinServoDar3 12
+#define pinServoDar1 4
+#define pinServoDar2 2
+#define pinServoDar3 15
 
 // put your setup code here, to run once:
 #define SenderTxPin 22
@@ -38,7 +39,21 @@ int indiceDroit2 = 0;
 int indiceDroit3 = 0;
 int indiceDroit4 = 0;
 
-int vitesse=30;
+int vitesse=90;
+
+  int posBaseDav1=90;
+  int posBaseDav2=35;
+  int posBaseDav3=15;
+  
+  int posBaseGm1=100;
+  int posBaseGm2=150;
+  int posBaseGm3=165;
+
+  int posBaseDar1=110;
+  int posBaseDar2=35;
+  int posBaseDar3=15;
+
+String recu;
 
 void setup() {
   Serial.begin(115200);
@@ -74,13 +89,27 @@ void setup() {
   Dar1.attach(pinServoDar1,500,2400);
   Dar2.attach(pinServoDar2,500,2400);
   Dar3.attach(pinServoDar3,500,2400);
+
+  //-------------------position de base des pattes au démarrage--------------------
+  Dav1.write(posBaseDav1);
+  Dav2.write(posBaseDav2);
+  Dav3.write(posBaseDav3);
+
+  Gm1.write(posBaseGm1);
+  Gm2.write(posBaseGm2);
+  Gm3.write(posBaseGm3);
+
+  Dar1.write(posBaseDar1);
+  Dar2.write(posBaseDar2);
+  Dar3.write(posBaseDar3);
 }
 
 
 void loop() {
+
   while(Esclave.available()){
-    String recu = Esclave.readString();
-    Serial.print(recu);
+    recu = Esclave.readStringUntil(';');
+    Serial.println(recu);
   
     if(recu=="avancer"){
       Serial.println("j'avance");
@@ -99,32 +128,32 @@ void loop() {
     }
     delay(vitesse);
   }
-  Serial.println("Pas de connexion...");
+
 }
 
 //C'est cette patte qui donne le rythme
 void moveLegDav(){
   // Rise the leg
   if (indiceDroit1 <= 10) {
-    Dav3.write(60 - indiceDroit1 * 2);
-    Dav2.write(90 - indiceDroit1 * 3);
+    Dav3.write(posBaseDav3 - (indiceDroit1*2));
+    Dav2.write(posBaseDav2 - (indiceDroit1*3));
     indiceDroit1++;
   }
   // Rotate the leg
   if (indiceDroit2 <= 30) {
-    Dav1.write(100 - indiceDroit2);
+    Dav1.write(posBaseDav1 - indiceDroit2);
     indiceDroit2++;
   }
   // Move back to touch the ground
   if (indiceDroit2 > 20 & indiceDroit3 <= 10) {
-    Dav3.write(40 + indiceDroit3 * 2);
-    Dav2.write(60 + indiceDroit3 * 3);
+    Dav3.write(posBaseDav3 + (indiceDroit3*2));
+    Dav2.write(posBaseDav2 + (indiceDroit3*3));
     indiceDroit3++;
   }
   // Stance phase - move leg while touching the ground
   // Rotate back to initial position
   if (indiceDroit2 >= 30) {
-    Dav1.write(70 + indiceDroit4);
+    Dav1.write((posBaseDav1-30) + indiceDroit4);  //Dav1 est à sa (posBase-30) pcq elle est à la position avancée. On la rétracte vers sa posBase (+30)
     indiceDroit4++;
   }
   // Reset the counters for repeating the process
@@ -140,49 +169,48 @@ void moveLegDav(){
 void moveLegDar() {
   //rise leg
   if (indiceDroit1 <= 10) {
-    Dar3.write(50 - indiceDroit1 * 2);
-    Dar2.write(80 - indiceDroit1 * 3);
+    Dar3.write(posBaseDar3 - (indiceDroit1*2));
+    Dar2.write(posBaseDar2 - (indiceDroit1*3));
   }
 
   //rotate vers l'avant
   if (indiceDroit2 <= 30) {
-    Dar1.write(80 - indiceDroit2);
+    Dar1.write(posBaseDar1 - indiceDroit2);
   }
 
   //en bas
   if (indiceDroit2 > 20 & indiceDroit3 <= 10) {
-    Dar3.write(30 + indiceDroit3 * 2);
-    Dar2.write(50 + indiceDroit3 * 3);
+    Dar3.write(posBaseDar3 + (indiceDroit3*2));
+    Dar2.write(posBaseDar2 + (indiceDroit3*3));
   }
 
   //rotate vers l'arrière
   if (indiceDroit2 >= 30) {
-    Dar1.write(50 + indiceDroit4);
+    Dar1.write((posBaseDar1-30) + indiceDroit4);  //Gar1 est à sa (posBase-30) pcq elle est à la position avancée. On la rétracte vers sa posBase (+30)
   }
 }
 
 void moveLegGm() {
   //haut
   if (indiceDroit1 <= 10) {
-    Gm3.write(80 + indiceDroit1 * 2);
-    Gm2.write(50 + indiceDroit1 * 3);
+    Gm3.write(posBaseGm3 + (indiceDroit1*2));
+    Gm2.write(posBaseGm2 + (indiceDroit1*3));
   }
 
   //rotate ++
   if (indiceDroit2 <= 30) {
-    Gm1.write(30 + indiceDroit2);
+    Gm1.write(posBaseGm1 + indiceDroit2);
 
   }
 
   //bas
   if (indiceDroit2 > 20 & indiceDroit3 <= 10) {
-    Gm3.write(100 - indiceDroit3 * 2);
-    Gm2.write(80 - indiceDroit3 * 3);
+    Gm3.write(posBaseGm3 - (indiceDroit3*2));
+    Gm2.write(posBaseGm2 - (indiceDroit3*3));
   }
 
   //rotate --
   if (indiceDroit2 >= 30) {
-    Gm1.write(60 - indiceDroit4);
+    Gm1.write((posBaseGm1+30) - indiceDroit4);  //Dm1 est à sa (posBase-30) pcq elle est à la position avancée. On la rétracte vers sa posBase (+30)
   }
 }
-
